d4fe4f488873e7617144b47f73fb6833df3921cd
Mary Jack <maryjackinthebox@gmail.com>
2018-04-05 01:26:35 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,8 @@
 def square_array(array)
-  # your code here
+ squares = Array.new 
+ array.each do |num|
+   num = num**2 
+   squares.push(num)
+  end
+  return squares #put return outside of do
 end
